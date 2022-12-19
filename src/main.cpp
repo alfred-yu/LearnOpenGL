69c67c9edb537efd0043ecb53f37673e2123e164
Yu Shaojian <shaojian.yu@hotmail.com>
2022-12-19 14:19:56 +0800

    1. Add draw elements support 2. Add polygon draw mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,31 @@ const string WINDOW_TITLE = "OpenGL";
 
 void framebuffersizefun(GLFWwindow *context, int width, int height);
 void processInput(GLFWwindow* context);
+void keyEventCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
+#if 0
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	 0.5f, -0.5f, 0.0f,
 	 0.0f,  0.5f, 0.0f
 };
+#else
+float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+
+unsigned int indices[] = {
+	// 注意索引从0开始! 
+	// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+	// 这样可以由下标代表顶点组合成矩形
+
+	0, 1, 3, // 第一个三角形
+	1, 2, 3  // 第二个三角形
+};
+#endif
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
@@ -56,6 +75,7 @@ int main(int argc, char* argv[])
 
 	glfwMakeContextCurrent(context);
 	glfwSetFramebufferSizeCallback(context, framebuffersizefun);
+	glfwSetKeyCallback(context, keyEventCallback);
 
 	// init glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -77,6 +97,14 @@ int main(int argc, char* argv[])
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
+
+#if 1
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+#endif
+
 	glBindVertexArray(0);
 
 	int success;
@@ -122,15 +150,16 @@ int main(int argc, char* argv[])
 
 	while (!glfwWindowShouldClose(context))
 	{
-		processInput(context);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
+#if 0
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+#else
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+#endif
 		glfwSwapBuffers(context);
 		glfwPollEvents();
 	}
@@ -147,8 +176,18 @@ void framebuffersizefun(GLFWwindow* context, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* context)
+void keyEventCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (glfwGetKey(context, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(context, GL_TRUE);
+	static bool showPolyonMode = false;
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	else if (key == GLFW_KEY_P && action == GLFW_PRESS)
+	{
+		showPolyonMode = !showPolyonMode;
+		cout << "showPolyonMode:" << showPolyonMode << endl;
+		glPolygonMode(GL_FRONT_AND_BACK, showPolyonMode ? GL_LINE : GL_FILL);
+	}
 }
