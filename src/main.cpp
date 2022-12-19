19cf6c3403df64b49c436e7b7d765084aa890022
Yu Shaojian <shaojian.yu@hotmail.com>
2022-12-19 14:26:59 +0800

    1. Add color with GLSL

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void framebuffersizefun(GLFWwindow *context, int width, int height);
 void processInput(GLFWwindow* context);
 void keyEventCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
-#if 0
+#if 1
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	 0.5f, -0.5f, 0.0f,
@@ -38,17 +38,22 @@ unsigned int indices[] = {
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
+"\n"
+"out vec4 vertexColor;\n"
+"\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\n";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "\n"
 "void main()\n"
 "{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	FragColor = vertexColor;\n"
 "}\n";
 
 
@@ -98,7 +103,7 @@ int main(int argc, char* argv[])
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
 
-#if 1
+#if 0
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -155,7 +160,7 @@ int main(int argc, char* argv[])
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-#if 0
+#if 1
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 #else
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
